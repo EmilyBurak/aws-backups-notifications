e76b30739d2dc30675ec529dc66bd55cec4b6291
Emily Burak <emilyburak@Emilys-MacBook-Pro.local>
2024-07-06 10:01:53 -0600

    Update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,5 @@
 output "ec2_id" {
-  value = aws_instance.web.id
-}
-
-output "backup_vault_arn" {
-  value = aws_backup_vault.backup_vault.arn
+  value = aws_instance.backup_test_instance.id
 }
 
 output "backup_vault_name" {
@@ -11,7 +7,7 @@ output "backup_vault_name" {
 }
 
 output "ec2_instance_arn" {
-  value = aws_instance.web.arn
+  value = aws_instance.backup_test_instance.arn
 }
 
 output "backup_default_role_arn" {
